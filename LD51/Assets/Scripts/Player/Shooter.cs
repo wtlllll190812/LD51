7228455ebe1565ff909e04a4262c87a0a6c0e17b
wtlllll <3200102777@zju.edu.cn>
2022-10-02 21:04:45 +0800

    玩家发射子弹

--- LD51/Assets/Scripts/Player/Shooter.cs
+++ LD51/Assets/Scripts/Player/Shooter.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+{   
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 200f;
+    public Camera cam;
+    Vector3 mousePos;
+    public Transform FireTransform;
+    
     void Update()
     {
-        
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
     }
+
+     void FixedUpdate()
+    {
+       Vector3 target = (mousePos - transform.position).normalized;
+       float angle = Mathf.Atan2(target.y,target.x)*Mathf.Rad2Deg - 90f;
+       FireTransform.eulerAngles = new Vector3(0,0,angle);
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab,FirePoint.position,FirePoint.rotation);
+        Rigidbody2D rb2D = bullet.GetComponent<Rigidbody2D>();
+        rb2D.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+   
 }
