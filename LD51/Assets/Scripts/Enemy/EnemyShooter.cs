72299412d82a7c768875000254d84561fd124058
用户名 <96603553+wyryyds@users.noreply.github.com>
2022-10-03 19:39:37 +0800

    Enemy
    Enemy

--- LD51/Assets/Scripts/Enemy/EnemyShooter.cs
+++ LD51/Assets/Scripts/Enemy/EnemyShooter.cs
@@ -6,16 +6,23 @@ public class EnemyShooter : EnemyBase
 {
     public GameObject BulletPrefab;
     public float attackInterval=2f;
+    public float bulletForce=200f;
     private float timer;
     public Transform shooter;
 
+    private void FixedUpdate()
+    {
+        Vector3 _target = (target.position - transform.position).normalized;
+        float angle = Mathf.Atan2(_target.y, _target.x) * Mathf.Rad2Deg - 90f;
+        shooter.eulerAngles = new Vector3(0, 0, angle);
+    }
     protected override void Attack()
     {
         timer += Time.deltaTime;
         if (timer > attackInterval)
         {
-            //Éú³É×Óµ¯
-            
+            GameObject bullet = Instantiate(BulletPrefab, shooter.position , shooter.rotation);
+            bullet.GetComponent<Bullet>().InitBullet(shooter.up * bulletForce, EBulletType.hitPlayer);
             timer = 0;
         }
     }
